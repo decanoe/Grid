abbae00b9aa8315b37cd846868841ca390ba6345
decanoe <deca.noe49@gmail.com>
2023-05-09 15:23:16 +0200

    main change for argv

--- main.cpp
+++ main.cpp
@@ -1,25 +1,76 @@
 #include <iostream>
 #include <string>
+#include <string.h>
+#include <time.h>
 #include "Functions/InputOutput.h"
 #include "Functions/Compute.h"
 #include "Class/PartialSolution.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    // Grid G = ReadGrid("./test_grids/probleme_12_a.txt");
-    Grid G = ReadGrid("./test_grids/grid_exemple.txt");
-    // G.Print();
-    // std::cout << "\n";
+    if (argc < 2)
+    {
+        std::cerr << "ERROR : No input grid were provided (use -h for help)";
+        return 0;
+    }
 
-    // Solution S = ReadSolution("./test_grids/solve_exemple.txt");
-    // S.Print();
+    if (strcmp(argv[1], "-h") == 0)
+    {
+        std::cout <<
+            "arguments :" << 
+            "\n\t<file>  \t: the path to the file containing the grid" <<
+            "\n\t-o <file>\t: give a file path on which write the result" <<
+            "\n\t-t <duration>\t: give a time limit (in seconds) to the calculation (default is 60s)";
+        return 0;
+    }
 
-    // std::cout << computeScore(G, S);
+    std::string inputPath = argv[1];
+    std::string outputPath = "";
+    int maxTime = 60;
+
+    for (int i = 2; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-o") == 0)
+        {
+            if (outputPath != "")
+            {
+                std::cerr << "ERROR : -o was used twice, only one output path is supported";
+                return 0;
+            }
+            if (i + 1 >= argc)
+            {
+                std::cerr << "ERROR : -o was used but no output path were provided after it";
+                return 0;
+            }
+            outputPath = argv[i + 1];
+        }
+        if (strcmp(argv[i], "-t") == 0)
+        {
+            if (maxTime != 10)
+            {
+                std::cerr << "ERROR : -t was used twice, only one duration is supported";
+                return 0;
+            }
+            if (i + 1 >= argc)
+            {
+                std::cerr << "ERROR : -t was used but no duration were provided after it";
+                return 0;
+            }
+            maxTime = std::stoi(argv[i + 1]);
+        }
+    }
+    
+    Grid G = ReadGrid(inputPath);
+
     Solution best = Solution(0);
     int best_score = 0;
-    int nb = 0;
-    for (int i = 0; i < 128*128; i++)
+
+    clock_t startTime = clock();
+    clock_t lastTime = 0;
+    while(clock() - startTime + lastTime < maxTime * CLOCKS_PER_SEC - lastTime * 10)
     {
+        clock_t t = clock();
+
         PartialSolution PS = PartialSolution(&G);
         Solution S = PS.Solve();
 
@@ -27,19 +78,21 @@ int main()
         if (best_score < score)
         {
             best_score = score;
-            nb = 1;
             best = S;
             std::cout << score << "\n";
         }
-        else if (score == best_score)
-            nb += 1;
 
         PS.Delete();
+
+        lastTime = clock() - t;
     }
 
     std::cout << "best grid :\n";
     best.Print();
-    std::cout << "\nscore : " << best_score << " (nb of possibility found : " << nb << ")";
+    std::cout << "\nscore : " << best_score << " (time spent : " << ((float)(clock() - startTime))/CLOCKS_PER_SEC << "s)";
+
+    if (outputPath != "")
+        WriteSolution(G, best, outputPath);
 
     return 0;
 }
